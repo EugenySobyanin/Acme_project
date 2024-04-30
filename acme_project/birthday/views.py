cab41dbfffd7c70d706f7857de367722dad66d71
Евгений Собянин <ea.sobyanin2018@gmail.com>
2024-05-01 00:22:42 +0300

    Добавлена пагинация

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, redirect, render
 
 from .forms import BirthdayForm
@@ -14,9 +15,9 @@ def birthday(request, pk=None):
         instance = None
     form = BirthdayForm(
         request.POST or None,
-        files=request.FILES or None, 
+        files=request.FILES or None,
         instance=instance)
-    context = {'form':form}
+    context = {'form': form}
     
     if form.is_valid():
         form.save()
@@ -30,8 +31,11 @@ def birthday(request, pk=None):
 
 
 def birthday_list(request):
-    birthdays = Birthday.objects.all()
-    context = {'birthdays': birthdays}
+    birthdays = Birthday.objects.all().order_by('id')
+    paginator = Paginator(birthdays, 5)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {'page_obj': page_obj}
     return render(request, 'birthday/birthday_list.html', context)
 
 
