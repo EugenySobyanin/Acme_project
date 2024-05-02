105311e12e5e405be6dd3e77395cb4589fb347de
Ускова Ксения Вадимовна <pc1013400360@DTDF.ru>
2024-05-02 17:54:02 +0300

    Начало работы над взаимодействиями с пользователями

--- acme_project/acme_project/urls.py
+++ acme_project/acme_project/urls.py
@@ -8,5 +8,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('birthday/', include('birthday.urls')),
     path('auth/', include('django.contrib.auth.urls')),
-    path('auth/', include('django.contrib.auth.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
